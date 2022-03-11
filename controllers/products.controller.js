c40e361cba96169a5cadf5871cdc799d56533161
mrevanzak <mrevanzak@gmail.com>
2022-03-11 19:30:49 +0700

    products route

---
+++ controllers/products.controller.js
@@ -0,0 +1,125 @@
+const Product = require("../models/Products");
+
+const createProduct = async (req, res) => {
+    try {
+        const product = await Product.findOne({
+            name: req.body.name,
+        });
+        if (product) {
+            return res.status(400).json({
+                msg: "Product already exists",
+            });
+        }
+        const newProduct = await Product.create(req.body);
+        res.status(201).json({
+            status: "success",
+            data: {
+                newProduct,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            message: err,
+        });
+    }
+};
+
+const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json({
+            status: "success",
+            data: {
+                products,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            message: err,
+        });
+    }
+};
+
+const getProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({
+                status: "fail",
+                message: "Product not found",
+            });
+        }
+        res.status(200).json({
+            status: "success",
+            data: {
+                product,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            message: err,
+        });
+    }
+};
+
+const updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+        if (!product) {
+            return res.status(404).json({
+                status: "fail",
+                message: "Product not found",
+            });
+        }
+        res.status(200).json({
+            status: "success",
+            data: {
+                product,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            message: err.message,
+        });
+    }
+};
+
+const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if (!product) {
+            return res.status(404).json({
+                status: "fail",
+                message: "Product not found",
+            });
+        }
+        res.status(200).json({
+            status: "success",
+            data: null,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            message: err,
+        });
+    }
+};
+
+module.exports = {
+    createProduct,
+    getProducts,
+    getProduct,
+    updateProduct,
+    deleteProduct,
+};
--- routes/products.route.js
+++ routes/products.route.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-  res.status(200).send('Hello Product');
-});
+const {
+    getProduct,
+    getProducts,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+} = require("../controllers/products.controller");
+
+router.route("/").get(getProducts).post(createProduct);
+router.route("/:id").get(getProduct).patch(updateProduct).delete(deleteProduct);
 
 module.exports = router;
