471ac7739823c4d25581e8ee9c65174c26243e97
Richo <daininoricho@yahoo.com>
2022-03-11 23:48:48 +0700

    Adding more unit testing to the orders service

--- services/orders.service.test.js
+++ services/orders.service.test.js
@@ -30,6 +30,33 @@ describe('Order service', () => {
     }
   }
   
+  const orderUpdate = {
+    total: 10,
+    status: "WAITING",
+    items: [
+      {
+        product: {
+          name: "Winger Combo BBQ",
+          price: "Rp. 22.999",
+          image: "https://i.ibb.co/RHJZ1Lj/carousel04.png",
+        },
+        quantity: 2
+      },
+    ],
+    delivery: {
+      address: {
+        namaJalan: "Jl. Kumara Sari",
+        nomorRumah: 16,
+        kelurahan: "Jimbaran",
+        kecamatan: "Kuta Selatan",
+        kabupaten: "Badung",
+        kodePos: 80361,
+      },
+      fullname: "Richo Dainino",
+      phone: "08113857878"
+    }
+  }
+  
   test('Create should return input data with id', async () => {
     const orderServiceInstance = new OrderService();
 
@@ -55,5 +82,42 @@ describe('Order service', () => {
     const result = await orderServiceInstance.getAll();
     expect(Order.find).toHaveBeenCalledTimes(1);
     expect(result).toEqual([]);
-   });
+  });
+
+  test('Get order by id should return empty array if no data is present at database', async () => { 
+    const orderServiceInstance = new OrderService();
+
+    Order.findById = jest.fn().mockResolvedValue([]);
+
+    const result = await orderServiceInstance.getById('622b3f962bb17dfdbecde546');
+    expect(Order.findById).toHaveBeenCalledTimes(1);
+    expect(result).toEqual([]);
+  });
+
+  test('Update should return the updated data', async () => { 
+    const orderServiceInstance = new OrderService();
+
+    Order.findOneAndUpdate = jest.fn().mockResolvedValue({
+      _id: '622b3f962bb17dfdbecde546',
+      ...orderUpdate
+    });
+
+    const result = await orderServiceInstance.updateById('622b3f962bb17dfdbecde546');
+
+    expect(Order.findOneAndUpdate).toHaveBeenCalledTimes(1);
+    expect(result).toEqual({
+      _id: expect.any(String),
+      ...orderUpdate,
+    });
+  });
+
+  test('Delete should return empty array if no data is present at database', async () => { 
+    const orderServiceInstance = new OrderService();
+
+    Order.findOneAndDelete = jest.fn().mockResolvedValue([]);
+
+    const result = await orderServiceInstance.deleteById();
+    expect(Order.findOneAndDelete).toHaveBeenCalledTimes(1);
+    expect(result).toEqual([]);
+  });
  });
