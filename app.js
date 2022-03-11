2730247d37b800683555cac54cb54aab2befbb64
Richo <daininoricho@yahoo.com>
2022-03-11 17:54:49 +0700

    Changing initial file, change of plan

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 require('dotenv').config();
 
 const connectDB = require('./db/connect');
-const accounts = require('./routes/accounts.route');
+const products = require('./routes/products.route');
+const orders = require('./routes/orders.route');
 
 const port = 5000;
 
@@ -12,16 +13,17 @@ app.use(express.json());
 // Untuk header application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1/accounts', accounts);
+app.use('/api/v1/products', products);
+app.use('/api/v1/orders', orders);
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello world! Check on localhost:5000/api/v1/accounts');
+  res.status(200).send('Hello world! Check on localhost:5000/api/v1/products');
 });
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`Server is running on localhost:${port}`));
+    app.listen(port, console.log(`Server is listening on localhost:${port}`));
   } catch (error) {
     console.log(error);
   }
--- routes/products.route.js
+++ routes/products.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/').get((req, res) => {
-  res.status(200).send('Hello Account');
+  res.status(200).send('Hello Product');
 });
 
 module.exports = router;
